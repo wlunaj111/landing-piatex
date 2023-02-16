c485322bbd6745a7328aef660dead9471874dbd8
Wilber <wlunaj111@gmail.com>
2023-02-16 10:24:26 -0500

    Adding environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+
+export const ip = 'https://piatex.mprc.cu/piatex';
+export const environment = {
+    production: true,
+    
+    api_url: 'https://piatex.mprc.cu/piatex',
+
+    login: `${ip}/o/token/`,
+    user: `${ip}/es/user/me/`,
+    register: `${ip}/v1/user/`,
+    verify_token: `${ip}/v1/user/verify_token/`,
+    permiso: `${ip}/v1/permision/`,
+    grupo: `${ip}/v1/group/`,
+    usuario: `${ip}/es/user/`,
+    usuariov1: `${ip}/v1/user/`,
+    advertiser: `${ip}/es/advertiser/`,
+    advertiserv1: `${ip}/v1/advertiser/`,
+    municipio: `${ip}/v1/municipality/`,
+    provincia: `${ip}/v1/province/`,
+    medio: `${ip}/v1/medio/`,
+    resumen: `${ip}/v1/resumen/datos/`,
+    zona: `${ip}/v1/zona/`,
+    ad: `${ip}/v1/ad/`,
+    payment: `${ip}/v1/payment/`,
+    payment_item: `${ip}/v1/payment_item/`,
+    clientId: 'SCBs6MhWpdArFZzJoojNkIcIf0iCFYXXSHB4uuGC',
+};
+
